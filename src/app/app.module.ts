5e74670fea1aaf40c824a7d89ac8719b6b5c3dc1
unknown <Administrateur@25SE23-4R0TVD3.ad.campus-eni.fr>
2022-06-22 16:27:17 +0200

    Config
    Add FormsModule & new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TeamComponent } from './team/team.component';
+import { PersonComponent } from './person/person.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TeamComponent,
+    PersonComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
